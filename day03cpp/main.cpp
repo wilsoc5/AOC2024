8e5f60913bcf486c9b58415843900f229751e069
Christopher Wilson <cgw@thewilsonadventure.com>
2024-12-02 23:49:55 -0600

    Working day3 p2.

--- day03cpp/main.cpp
+++ day03cpp/main.cpp
@@ -18,33 +18,78 @@ int main(int argc, char* argv[]){
     string line;
     uint64_t line_count = 0;
     int64_t sum = 0;
+    int64_t p2sum = 0;
+    const string delimiter = "mul(";
+    const string delimiter1 = "do()";
+    const string delimiter2 = "don't()";
+
+    bool p2enabled = true;
     while(getline(in,line)){
         if (line.empty()){
             continue;
         }
         
-        string delimiter = "mul(";
-
         while (! line.empty()){
-        auto pos = line.find(delimiter);
-        string token = line.substr(0, pos);
-        stringstream num (token);
-        int n1 = -1;
-        int n2 = -1;
-        char comma, paren;
-        num >> n1 >> comma >>n2 >>paren;
-        // Add the token to the array
-        cout <<token  << " | " <<n1 <<" | "<<comma <<" | " <<n2; 
-        if (num && (comma == ',') && (paren == ')')){
-            cout << "\t= " <<n1 * n2;
-            sum += n1 * n2;
-        }
-        cout <<endl;
-        //if (pos != string::npos) 
-        line.erase(0,pos + delimiter.length());
+            auto posmul = line.find(delimiter);
+            auto posdo = line.find(delimiter1);
+            auto posdont = line.find(delimiter2);
+
+            cout <<"LINE: " <<line <<"\t posmul: " <<posmul <<"\t posdo: " <<posdo <<"\tposdont: " << posdont <<endl;
+            if ((posmul < posdo) && (posmul < posdont)){
+                auto pos = posmul;
+                string token = line.substr(pos + delimiter.length(), -1);
+                stringstream num (token);
+                int n1 = -1;
+                int n2 = -1;
+                char comma, paren;
+                num >> n1 >> comma >>n2 >>paren;
+                cout <<"TOKEN: " <<token  << " | " <<n1 <<" | "<<comma <<" | " <<n2; 
+                if (num && (comma == ',') && (paren == ')')){
+                    cout << "\t= " <<n1 * n2;
+                    sum += n1 * n2;
+                    if ((posmul < posdont) && (posmul < posdo)){
+                        if (p2enabled){
+                            p2sum += n1*n2;
+                            cout <<"\t P2sum enabled";
+                        } 
+                    }
+                    else if (posmul < posdo){
+                        if (p2enabled){
+                            p2sum += n1*n2;
+                            cout <<"\t P2sum enabled";
+                        }
+                    }
+                    else if ((posdont < posmul) && (posdont < posdo)){
+                        p2enabled = false;
+                        cout <<"\t P2sum disabled";
+
+                    }
+                    else if (!p2enabled && (posdo < posmul)){
+                        p2enabled = true;
+                        p2sum += n1 * n2;
+                        cout <<"\t P2sum ensabled";
+                    }
+                }
+                cout <<endl;
+                line.erase(0,pos + delimiter.length());
+            }
+            else if ((posdo < posmul) && (posdo < posdont)){
+                auto pos = posdo;
+                line.erase(0,pos + delimiter1.length());
+                p2enabled = true;
+            }
+            else if ((posdont < posmul) && (posdont < posdo)){
+                auto pos = posdont;
+                line.erase(0,pos + delimiter2.length());
+                p2enabled = false;
+            }
+            else {
+                line.erase(0,posmul + delimiter.length());
+            }
         }
     }
     cout  <<"Sum = " <<sum <<endl;
+    cout  <<"Sum P2 = " <<p2sum <<endl;
 
     return 0;
 }
